ecbebd4e077fb4be0dbc690235bebc37427e1412
Albert <jack25512909@gmail.com>
2022-01-22 23:52:45 +0800

    看一下寫在裡面的備註。

--- practice/require01.js
+++ practice/require01.js
@@ -1,5 +1,5 @@
-const f1 = require("./arrow-func");
-const f2 = require("./arrow-func");
+const f1 = require("./arrow-func");  // 「.」就是目前所在的資料夾
+const f2 = require(__dirname + "/arrow-func"); // 也可以改成這樣寫。
 //* 第一次的require會去"./arrow-func" 找叫做f1的變數，因為我在"require01.js"這隻檔案用了f1個變數。
 //! 特別注意！第一次require 跟 第二次require不同
 //! 第二次require會先去檢查這隻檔案「先前」有沒有require過， 如果有的話會參照他然後設定給「新的變數」。
@@ -8,3 +8,4 @@ const f2 = require("./arrow-func");
 console.log(f1(9));
 console.log(f2(10));
 console.log("2 ", __dirname);
+console.log(f1 === f2); // 會得到true
