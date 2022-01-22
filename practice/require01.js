6ab069e9e670b7d5f42e49a2dfadd89993261341
Albert <jack25512909@gmail.com>
2022-01-22 23:41:28 +0800

    所以不會發生說require執行了兩次這件事。

--- practice/require01.js
+++ practice/require01.js
@@ -3,6 +3,7 @@ const f2 = require("./arrow-func");
 //* 第一次的require會去"./arrow-func" 找叫做f1的變數，因為我在"require01.js"這隻檔案用了f1個變數。
 //! 特別注意！第一次require 跟 第二次require不同
 //! 第二次require會先去檢查這隻檔案「先前」有沒有require過， 如果有的話會參照他然後設定給「新的變數」。
+//! 所以不會發生說require執行了兩次這件事。
 
 console.log(f1(9));
 console.log(f2(10));
