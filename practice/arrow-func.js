8972995d15f81b2cd08e2b1190e46f062ee0ec0c
Albert <jack25512909@gmail.com>
2022-01-23 00:00:35 +0800

    如果要同時匯出兩個

--- practice/arrow-func.js
+++ practice/arrow-func.js
@@ -1,6 +1,9 @@
 const f1 = a => a * a;
+const f3 = a => a * a * a;
 
-console.log(f1(7));
+// console.log(f1(7));
+// console.log("1 ", __dirname);
 
-module.exports = f1; // 匯出
-
+//module.exports = f1; // 匯出
+//module.exports = [f1, f3]; // 可以匯出array~~
+module.exports = {f1, f3}; // 不過一般都是匯出object
