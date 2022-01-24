cc8c02d6ad14a6a6e80f6d063216b4c4ad57215e
Albert <jack25512909@gmail.com>
2022-01-24 21:17:51 +0800

    自訂404的頁面

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 require('dotenv').config(); // 載入 .env 的設定    本身require進來就是一個object，『require('dotenv').config()』直接這樣寫就可以了。這一行要放在最前面。
 
-const express = require('express');   // 第一個動作 引入express。  『express』他是一個套件安裝進來的，所以不要給路徑。
+const express = require('express');   
 
-const app = express();  // 第二個動作 建立一個app『web server』，把它當作func來使用
+const app = express();  
 
 
-//* app.get() 這個get指的是http方法的get
-
-app.get('/', (req, res)=>{   //! 第三個動作 定義路由，有兩個重要的地方，一個是路徑另一個是方法   // 『 '/' 』 就是根目錄的意思
-    res.send(`<h2>Hello</h2>`);  // send() 如果是字串的話會看成html
+// 路由定義開始: Begin
+app.get('/', (req, res)=>{   
+    res.send(`<h2>Hello</h2>`);  
 })
 
-let port = process.env.PORT || 3000;   // 『 port = process.env.PORT 』是從'dotenv'過來的
-app.listen(port, ()=>{   // 第四個動作 下一個listen來偵聽
+// 其他的路由就定義在 app.get() 和 app.use() 的中間。 這裡擺放是有順序的 app.use(res.status(404)) 一定要擺在最後面，不然他會擋到其他路由。
+// 設定在前面的就是優先使用。 原則上我要優先的路由就放前面。
+
+
+// 路由定義結束: End
+app.use('/', (req, res)=>{
+    res.status(404).send(`<h1>找不到頁面</h1>`);
+})
+
+app.listen(port, ()=>{
     console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
     console.log(`啟動: ${port}`, new Date());
 });  
