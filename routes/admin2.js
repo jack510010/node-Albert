92082df3abd96c20417ca15f4d6c1d3816ff36f1
Albert <jack25512909@gmail.com>
2022-01-31 19:23:25 +0800

    寫在根目錄的寫法 顯示 url: req.url, baseUrl: req.baseUrl, 與admin3的寫法有差

--- routes/admin2.js
+++ routes/admin2.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.get('/admin2/:p1?/:p2?', (req, res) => {
     res.json({
         params: req.params,
+        url: req.url,
+        baseUrl: req.baseUrl,
     });
 });
 
