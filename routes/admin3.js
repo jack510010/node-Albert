e62c4919df105ca2c9aba47cce2cf068e399b7d2
Albert <jack25512909@gmail.com>
2022-01-31 19:24:53 +0800

    有指定路徑的寫法 不是寫在根目錄 顯示 url: req.url, baseUrl: req.baseUrl, 與admin2的寫法不同

---
+++ routes/admin3.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// 路由模組化
+const router = express.Router();
+
+router.get('/:p3?/:p4?', (req, res) => {    // module 裡面的路徑用相對路徑
+    res.json({
+        params: req.params,
+        url: req.url,
+        baseUrl: req.baseUrl,
+    });
+});
+
+module.exports = router;  // 匯出router， 原則上這就是一個middleware。
+
+
