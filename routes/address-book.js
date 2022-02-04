127cb5cb76826fddb5cd4c1562f01319387879cb
Albert <jack25512909@gmail.com>
2022-02-05 00:17:12 +0800

    呈現分頁。 分頁的page第幾頁要直接打在網址列。?page=

--- routes/address-book.js
+++ routes/address-book.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/list', async(req, res) => {
-    const perPage = 5;
+    const perPage = 5;  // 每一頁有幾筆
     let page = parseInt(req.query.page) || 1;
 
     const output = {
@@ -19,7 +19,7 @@ router.get('/list', async(req, res) => {
     const totalSql = `SELECT COUNT(1) totalRows FROM address_book`  // 可以去phpMyAdmin 的 sql那邊把語法寫好之後，在這裡貼上就不怕寫錯。
     const [[{totalRows}]] = await db.query(totalSql);
     output.totalRows = totalRows;
-    
+
     /*
     ! 一開始是這樣寫的 const [result1]  output.result1 = result1
     ! 不是 const [[{totalRows}]]      output.totalRows = totalRows
@@ -28,6 +28,25 @@ router.get('/list', async(req, res) => {
     ! 會改寫成 [[result1]] ，然後還要再解開來，要裡面的 totalRows 
     ! 所以會寫成 [[{totalRows}]]
     */
+
+    output.totalPages = Math.ceil(totalRows / perPage);  // 求總頁數
+    output.perPage = perPage;  // 每一頁有幾筆
+    output.rows = [];
+    output.page = page;
+    
+
+    if(totalRows > 0){
+        if(page < 1){
+            return res.redirect('?page=1');
+        }
+        if(page > output.totalPages){
+            return res.redirect('?page=' + output.totalPages);
+        }
+        const sql = `SELECT * FROM address_book LIMIT ${(page - 1) * perPage}, ${perPage}`;
+        const [rows] = await db.query(sql);
+        output.rows = rows;
+
+    }
 
     res.json(output);
     //res.render('address-book/list');
